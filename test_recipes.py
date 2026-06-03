fbcd524fdbaf4a3ebb3c7d754f680de1781df97c
Муханова Мелания <msmukhanova@edu.hse.ru>
2026-06-03 15:29:04 +0300

    Два теста: один проверяет, что возвращается строка в нужном формате, другой - что два ингредиента считаются одинаковыми, если у них совпадают name, unit

--- test_recipes.py
+++ test_recipes.py
@@ -5,3 +5,12 @@ def ingred_correct_init():
     assert one.name == "Мука"
     assert one.quantity == 500.0
     assert one.unit == "Г"
+
+def ingred_str_correct():
+    one = Ingredient("Мука", 500, "Г")
+    assert str(one) == "Мука: 500.0 г"
+
+def ingred_eq_same_nu():
+    one = Ingredient("Мука", 500, "Г")
+    other = Ingredient("Мука", 300, "Г")
+    assert one == other
